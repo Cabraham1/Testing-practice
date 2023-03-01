882d27e64745d458ca705d22141e414f3f6665d3
cabraham1 <christopherabraham8@gmail.com>
2023-03-01 18:59:39 +0100

    pass test 3

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
 // test 1
 
-const stringLength=(string) => {
+const stringLength = (string) => {
   if (string.length > 0 && string.length < 10) {
     return string.length;
+  } else {
+    throw new Error("string length is not between 0 to 10");
   }
-  else {
-    throw new Error('string length is not between 0 to 10')
-  }
-}
-
+};
 
 const reverseString = (string) => {
-  return string.split('').reverse().join('')
+  return string.split("").reverse().join("");
+};
+
+class Calculator {
+  constructor(num1, num2) {
+    this.num1 = num1;
+    this.num2 = num2;
+  }
+
+  add() {
+    return this.num1 + this.num2;
+  }
+  subtract() {
+    return this.num1 - this.num2;
+  }
 }
 
-module.exports = { stringLength, reverseString };
+module.exports = { stringLength, reverseString, Calculator };
--- script.test.js
+++ script.test.js
@@ -26,11 +26,32 @@ test("Should return Error if string count isnt between 0 & 10", () => {
   expect(actual).toThrowError();
 }); //this is a function that throws an error
 
-
-test('should reture reversed strings', () => {
-  const string = "hello world"
+test("should reture reversed strings", () => {
+  const string = "hello world";
 
   const actual = reverseString(string);
   //assert
   expect(actual).toBe(reverseString(string));
-})
+});
+
+test("1 + 2 should return 2", () => {
+  //arrange
+  const num1 = 1;
+  const num2 = 2;
+  //act
+  const calculator = new Calculator(num1, num2);
+  const actual = calculator.add();
+  //assert
+  expect(actual).toBe(3);
+});
+
+test("2 - 1 should return 1", () => {
+  //arrange
+  const num1 = 2;
+  const num2 = 1;
+  //act
+  const calculator = new Calculator(num1, num2);
+  const actual = calculator.subtract();
+  //assert
+  expect(actual).toBe(1);
+});
